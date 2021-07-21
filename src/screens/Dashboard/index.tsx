48e9eadf8272609df2d2be446a7135183a504815
matheusgouveia <matheus_guitarplayer@hotmail.com>
2021-07-21 19:04:50 -0300

    adicionar fontes e criar o Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,38 @@
 import React from "react";
-import { Text } from "react-native";
 
-import { Container, Title } from "./styles";
+import {
+	Container,
+	Header,
+	UserWrapper,
+	UserInfo,
+	Photo,
+	User,
+	UserGreeting,
+	UserName,
+	Icon,
+} from "./styles";
 
 export function Dashboard() {
 	return (
 		<Container>
-			<Title>Dashboard</Title>
+			<Header>
+				<UserWrapper>
+					<UserInfo>
+						<Photo
+							source={{
+								uri: "https://avatars.githubusercontent.com/u/31319822?v=4",
+							}}
+						/>
+
+						<User>
+							<UserGreeting>Olá,</UserGreeting>
+							<UserName>Matheus</UserName>
+						</User>
+					</UserInfo>
+
+					<Icon name="power" />
+				</UserWrapper>
+			</Header>
 		</Container>
 	);
 }
