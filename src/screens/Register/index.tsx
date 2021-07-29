963b7eaaa4f06f264a15d82462c0e8b2bb3d657a
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2021-07-28 23:10:45 -0300

    estilizar botâo de tipo da transação

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Input } from "../../components/Form/Input";
 import { Button } from "../../components/Form/Button";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 
-import { Container, Header, Title, Form, Fields } from "./styles";
+import {
+	Container,
+	Header,
+	Title,
+	Form,
+	Fields,
+	TransactionTypes,
+} from "./styles";
 
 export function Register() {
+	const [transactionType, setTransactionType] = useState("");
+
+	function handleTransactionTypeSelect(type: "up" | "down") {
+		setTransactionType(type);
+	}
+
 	return (
 		<Container>
 			<Header>
@@ -17,6 +31,22 @@ export function Register() {
 					<Input placeholder="Nome" />
 
 					<Input placeholder="Preço" />
+
+					<TransactionTypes>
+						<TransactionTypeButton
+							isActive={transactionType === "up"}
+							type="up"
+							title="Income"
+							onPress={() => handleTransactionTypeSelect("up")}
+						/>
+
+						<TransactionTypeButton
+							isActive={transactionType === "down"}
+							type="down"
+							title="Outcome"
+							onPress={() => handleTransactionTypeSelect("down")}
+						/>
+					</TransactionTypes>
 				</Fields>
 
 				<Button title="Enviar" />
